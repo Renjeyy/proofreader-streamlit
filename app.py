50df8659ee1d3ef06e316668f9b93a6a1fe05841
Renjeyy <renzie.aditya@ui.ac.id>
2025-09-12 09:47:38 +0700

    Update app.py

--- app.py
+++ app.py
@@ -102,7 +102,8 @@ def proofread_with_gemini(text_to_check):
     18. Kalau ada kata "email", biarkan itu sebagai benar tapi disarankan italic saja
     19. Untuk nama modul seperti "Modul Sourcing, dll", itu tidak perlu italic
     20. Kalau ada kata dalam bahasa inggris yang masih masuk akal dan nyambung dengan kalimat yang dibahas, tidak perlu Anda sarankan untuk ganti ke bahasa indonesia
-    21. Jika ada bahasa inggris dan akronimnya seperti "General Ledger (GL)", tolong dilakukan italic pada kata tersebut pada saat download file hasil revisinya, akronimnya tidak usah
+    21. Jika ada bahasa inggris dan akronimnya seperti "General Ledger (GL)", tolong dilakukan italic pada kata tersebut pada saat download file hasil revisinya, akronimnya tidak usah\
+    22. Awal kalimat selalu dimulai dengan huruf kapital
 
     PENTING: Berikan hasil dalam format yang SANGAT KETAT. Untuk setiap kesalahan, gunakan format:
     [SALAH] kata atau frasa yang salah -> [BENAR] kata atau frasa perbaikan -> [KALIMAT] kalimat lengkap asli tempat kesalahan ditemukan
@@ -546,6 +547,7 @@ if 'coherence_results' in st.session_state:
         }, inplace=True)
 
         st.dataframe(df_coherence, use_container_width=True)
+
 
 
 
