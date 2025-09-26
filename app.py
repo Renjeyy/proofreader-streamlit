7870d1dced0d3de08804576ea908341a2cc73740
Renjeyy <renzie.aditya@ui.ac.id>
2025-09-26 10:49:19 +0700

    Update app.py

--- app.py
+++ app.py
@@ -24,8 +24,8 @@ except Exception:
 st.markdown("<h1 style='text-align: center;'>Website Proofreader COE Divisi SKAI IFG</h1>", unsafe_allow_html=True)
 st.markdown(
     """
-    <div style="border: 1px solid #ddd; border-radius: 5px; padding: 15px; margin-bottom: 20px;">
-        <p style="color:red; font-weight:bold; text-align:center; font-size: 1.1em;">
+    <div style="border: 5px solid #ddd; border-radius: 5px; padding: 15px; margin-bottom: 20px;">
+        <p style="color:red; font-weight:bold; text-align:center; font-size: 1.8em;">
             Sebelum Menggunakan Fitur di Bawah Ini
         </p>
         <p style="text-align: justify;">
@@ -693,6 +693,7 @@ if 'recommendations' in st.session_state:
                 mime="application/vnd.openxmlformats-officedocument.wordprocessingml.document",
                 use_container_width=True
             )
+
 
 
 
